4517c927ff652dc1c0139a74e3f5d4b76ffefa1a
Pavel Lutskov <pavel.lutskov@gmail.com>
2018-12-07 14:21:08 +0100

    did more stuff

--- server.py
+++ server.py
@@ -23,11 +23,13 @@ def rec_for_user(uid):
     global model
 
     # Get dummy predictions
-    predictions = model.predict([5., 6., 7., 0., 7., 8., 7., 0., 8., 5.], top_courses=3)
+    predictions = model.predict([5., 6., 7., 0., 7., 8., 7., 0., 8., 5.],
+                                top_courses=3)
 
     recs = [
-        {'name': n, 'score': s} for n, s in 
-        zip(['BWL I', 'BWL II', 'VWL I'], [predictions[0], predictions[1], predictions[2]])
+        {'name': n, 'score': s} for n, s in
+        zip(['BWL I', 'BWL II', 'VWL I'],
+            [predictions[0], predictions[1], predictions[2]])
     ]
     ###
 
@@ -55,10 +57,11 @@ def do_vote(uid):
 
 
 if __name__ == '__main__':
-	# Load and prepare ML Model
+    # Load and prepare ML Model
     global model
     model = MLModel()
-    predictions = model.predict([5., 6., 7., 0., 7., 8., 7., 0., 8., 5.], top_courses=3)
+    predictions = model.predict([5., 6., 7., 0., 7., 8., 7., 0., 8., 5.],
+                                top_courses=3)
     ###
 
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -2,31 +2,35 @@ console.log('yey');
 var vm = new Vue({
     el: "#main-app",
     data: {
+        votes: {},
+        recommendations: [],
+        avail_courses: ['BWLI', 'BWLII', 'VWLI'],
+        crt_course : '',
+        crt_grade : '',
         outlist: [],
-        do_vote: false,
-        avail_courses: ['hui', 'govno']
     },
+
     methods: {
         get_recs: function () {
             this.do_vote = false;
             var self = this;
-            $.getJSON(
-                '/users/0/recommendations'
-            ).then(function (response) {
+            $.getJSON({
+                url:'/users/0/recommendations',
+                data: this.votes
+            }).then(function (response) {
                 self.outlist = response;
             });
         },
-        get_votes: function () {
-            this.do_vote = false;
-            var self = this;
-            $.getJSON(
-                '/users/0/votes'
-            ).then(function (response) {
-                self.outlist = response;
-            });
-        },
-        display_vote: function () {
-            this.do_vote = true;
+
+        add_vote: function () {
+            if (!this.crt_course || !this.crt_grade) return;
+            console.log('here');
+            this.votes[this.crt_course] = this.crt_grade;
+            this.avail_courses = this.avail_courses.filter(
+                function (el) { return el !== this.crt_course; }
+            );
+            this.crt_course = '';
+            this.crt_grade = '';
         }
     }
 });
