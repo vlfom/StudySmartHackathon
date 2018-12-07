8ebf483bae984f6e93df5a1b49303e2f427bb919
Pavel Lutskov <pavel.lutskov@gmail.com>
2018-12-07 16:53:46 +0100

    fixed fuckup

--- static/index.js
+++ static/index.js
@@ -1,8 +1,13 @@
 console.log('yey');
-var rec_vm = new Vue({
-    el: "#rec-app",
+var vm = new Vue({
+    el: "#main-app",
     data: {
+        votes: {},
         recommendations: [],
+        avail_courses: ['BWLI', 'BWLII', 'VWLI'],
+        crt_course : '',
+        crt_grade : '',
+        outlist: [],
     },
 
     methods: {
@@ -14,7 +19,34 @@ var rec_vm = new Vue({
                 self.outlist = response;
             });
         },
+
+        get_votes: function () {
+            var self = this;
+            $.getJSON({
+                url:'/users/0/votes',
+            }).then(function (response) {
+                self.votes = response;
+            });
+        },
+
+        add_vote: function () {
+            if (!this.crt_course || !this.crt_grade) return;
+            this.votes[this.crt_course] = this.crt_grade;
+            // this.avail_courses = this.avail_courses.filter(
+                // function (el) { return el !== this.crt_course; }
+            // );
+            var jdata = {};
+            jdata[this.crt_course] = this.crt_grade;
+            console.log(jdata);
+            $.post({
+                url: '/users/0/vote',
+                data: jdata,
+                dataType: 'json'
+            });
+            this.crt_course = '';
+            this.crt_grade = '';
+        }
     }
 });
 
- rec_vm.get_recs();
+ vm.get_votes();
--- static/recommendations.js
+++ static/recommendations.js
@@ -1,13 +1,8 @@
 console.log('yey');
-var vm = new Vue({
-    el: "#main-app",
+var rec_vm = new Vue({
+    el: "#rec-app",
     data: {
-        votes: {},
         recommendations: [],
-        avail_courses: ['BWLI', 'BWLII', 'VWLI'],
-        crt_course : '',
-        crt_grade : '',
-        outlist: [],
     },
 
     methods: {
@@ -19,34 +14,7 @@ var vm = new Vue({
                 self.outlist = response;
             });
         },
-
-        get_votes: function () {
-            var self = this;
-            $.getJSON({
-                url:'/users/0/votes',
-            }).then(function (response) {
-                self.votes = response;
-            });
-        },
-
-        add_vote: function () {
-            if (!this.crt_course || !this.crt_grade) return;
-            this.votes[this.crt_course] = this.crt_grade;
-            // this.avail_courses = this.avail_courses.filter(
-                // function (el) { return el !== this.crt_course; }
-            // );
-            var jdata = {};
-            jdata[this.crt_course] = this.crt_grade;
-            console.log(jdata);
-            $.post({
-                url: '/users/0/vote',
-                data: jdata,
-                dataType: 'json'
-            });
-            this.crt_course = '';
-            this.crt_grade = '';
-        }
     }
 });
 
- vm.get_votes();
+ rec_vm.get_recs();
