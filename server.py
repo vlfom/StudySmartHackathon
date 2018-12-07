48b429bd194bca14c5d83d4fbc0ed490d2c23d90
Pavel Lutskov <pavel.lutskov@gmail.com>
2018-12-07 11:50:57 +0100

    some shit happening

--- server.py
+++ server.py
@@ -1,60 +1,25 @@
-#!/usr/bin/python
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-from os import curdir, sep
+from flask import Flask, jsonify
 
-PORT_NUMBER = 8080
 
-#This class will handles any incoming request from
-#the browser 
-class myHandler(BaseHTTPRequestHandler):
-	
-	#Handler for the GET requests
-	def do_GET(self):
-		if self.path == "/":
-			self.path = "/index.html"
+app = Flask(__name__, template_folder='.')
 
-		try:
-			#Check the file extension required and
-			#set the right mime type
 
-			sendReply = False
-			if self.path.endswith(".html"):
-				mimetype = 'text/html'
-				sendReply = True
-			if self.path.endswith(".jpg"):
-				mimetype = 'image/jpg'
-				sendReply = True
-			if self.path.endswith(".gif"):
-				mimetype = 'image/gif'
-				sendReply = True
-			if self.path.endswith(".js"):
-				mimetype = 'application/javascript'
-				sendReply = True
-			if self.path.endswith(".css"):
-				mimetype = 'text/css'
-				sendReply = True
+@app.route('/users/<uid>/recommendations')
+def rec_for_user(uid):
+    recs = [
+        {'name': n, 'score': s} for n, s in zip(
+            ['BWL I', 'BWL II', 'VWL I'],
+            [0.95, 0.93, 0.8]
+        )
+    ]
+    return jsonify(recs)
 
-			if sendReply == True:
-				f = open(curdir + sep + self.path) 
-				self.send_response(200)
-				self.send_header('Content-type',mimetype)
-				self.end_headers()
-				self.wfile.write(f.read())
-				f.close()
-			return
 
-		except IOError:
-			self.send_error(404,'File Not Found: %s' % self.path)
+@app.route('/users/<uid>/votes')
+def votes_for_user(uid):
+    recs = [('Buchhaltung', 4), ('Huesos', 2)]
+    return jsonify(recs)
 
-try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port ' , PORT_NUMBER
-	
-	#Wait forever for incoming htto requests
-	server.serve_forever()
 
-except KeyboardInterrupt:
-	print '^C received, shutting down the web server'
-	server.socket.close()
+if __name__ == '__main__':
+    app.run(debug=True)
