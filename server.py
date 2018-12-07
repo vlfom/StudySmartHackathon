24736ce3a337c58baadff482a543cb12758498d7
Volodymyr Fomenko <vlfomenk@gmail.com>
2018-12-07 13:40:08 +0100

    Add ML template

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
 from flask import Flask, jsonify
 
+class MLModel():
+    def __init__(self):
+        pass
+
+    def predict(self, user_ratings, top_courses):
+        return user_ratings
+
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -11,12 +18,17 @@ def main():
 
 @app.route('/users/<uid>/recommendations')
 def rec_for_user(uid):
+    global model
+
+    # Get dummy predictions
+    predictions = model.predict([5., 6., 7., 0., 7., 8., 7., 0., 8., 5.], top_courses=3)
+
     recs = [
-        {'name': n, 'score': s} for n, s in zip(
-            ['BWL I', 'BWL II', 'VWL I'],
-            [0.95, 0.93, 0.8]
-        )
+        {'name': n, 'score': s} for n, s in 
+        zip(['BWL I', 'BWL II', 'VWL I'], [predictions[0], predictions[1], predictions[2]])
     ]
+    ###
+
     return jsonify(recs)
 
 
@@ -31,4 +43,10 @@ def votes_for_user(uid):
 
 
 if __name__ == '__main__':
+	# Load and prepare ML Model
+    global model
+    model = MLModel()
+    predictions = model.predict([5., 6., 7., 0., 7., 8., 7., 0., 8., 5.], top_courses=3)
+    ###
+
     app.run(debug=True)
